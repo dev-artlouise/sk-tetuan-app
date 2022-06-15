a4465210d1cf33eb954f909ab1b4c8c464844339
suhart tan <qeazdysxt1@gmail.com>
2022-06-15 21:59:13 +0800

    youth controller and model updated

--- app/Http/Controllers/YouthController.php
+++ app/Http/Controllers/YouthController.php
@@ -31,8 +31,37 @@ class YouthController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'firstName' => 'required',
-            'lastName' => 'required'
+            'fname' => 'required',
+            'lname' => 'required',
+            'mname' => 'required',
+            'datebirth' => 'required',
+            'birth_place' => 'required',
+            'contact_no' => 'required',
+            'address' => 'required',
+            'zone' => 'required',
+            'gender' => 'required',
+            'kk_member' => 'required',
+            'civil_status' => 'required',
+            'tribe' => 'required',
+            'religion' => 'required',
+            'email' => 'required',
+            'educational_attainment' => 'required',
+            'occupation' => 'required',
+            'youth_sector' => 'required',
+            'organization' => 'required',
+            'fathers_name' => 'required',
+            'fathers_occupation' => 'required',
+            'mothers_name' => 'required',
+            'mothers_occupation' => 'required',
+            'living_w_parents' => 'required',
+            'concerns_issue' => 'required',
+            'control_no' => 'required',
+            'batch' => 'required',
+            'civil_status_specify' => 'required',
+            'spouse_name' => 'required',
+            'youth_sec_specify' => 'required',
+            'org_specify' => 'required',
+            'encoded_by'=> 'required',
         ]);
 
         return Youth::create($request->all());
